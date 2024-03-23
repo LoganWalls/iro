1263f1cbd878e1d4c5d4e14f6137d848471791c0
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-03-23 19:35:47 -0400

    setup treesitter JS functions immediately

--- web/treesitter/highlighting.js
+++ web/treesitter/highlighting.js
@@ -1,57 +1,55 @@
-window.addEventListener("load", async () => {
-  async function setLanguage(name) {
-    // Initialize tree-sitter if needed
-    if (!window.TS.parser) {
-      await window.TreeSitter.init();
-      window.TS.parser = new window.TreeSitter();
-    }
-
-    // Load the language parser & queries
-    const language = await window.TreeSitter.Language.load(
-      `${document.baseURI}treesitter/${name}/grammar.wasm`,
-    );
-    const response = await fetch(
-      `${document.baseURI}treesitter/${name}/highlights.scm`,
-    );
-    const highlightQueries = await response.text();
-    await window.TS.parser.setLanguage(language);
-    window.TS.activeLanguage = language;
-    window.TS.query = language.query(highlightQueries);
+async function setLanguage(name) {
+  // Initialize tree-sitter if needed
+  if (!window.TS.parser) {
+    await window.TreeSitter.init();
+    window.TS.parser = new window.TreeSitter();
   }
 
-  // Highlighting code adapted from https://adrian.schoenig.me/blog/2022/05/27/tree-sitter-highlighting-in-jekyll/
-  function highlight(code) {
-    const tree = window.TS.parser.parse(code);
-    var adjusted = "";
-    var lastEnd = 0;
-    TS.query.matches(tree.rootNode).forEach((match) => {
-      const className = match.captures[0].name.replaceAll(".", "-");
-      const text = match.captures[0].node.text;
-      const start = match.captures[0].node.startIndex;
-      const end = match.captures[0].node.endIndex;
+  // Load the language parser & queries
+  const language = await window.TreeSitter.Language.load(
+    `${document.baseURI}treesitter/${name}/grammar.wasm`,
+  );
+  const response = await fetch(
+    `${document.baseURI}treesitter/${name}/highlights.scm`,
+  );
+  const highlightQueries = await response.text();
+  await window.TS.parser.setLanguage(language);
+  window.TS.activeLanguage = language;
+  window.TS.query = language.query(highlightQueries);
+}
 
-      if (start < lastEnd) {
-        return; // avoid duplicate matches for the same text
-      }
-      if (start > lastEnd) {
-        adjusted += code.substring(lastEnd, start);
-      }
+// Highlighting code adapted from https://adrian.schoenig.me/blog/2022/05/27/tree-sitter-highlighting-in-jekyll/
+function highlight(code) {
+  const tree = window.TS.parser.parse(code);
+  var adjusted = "";
+  var lastEnd = 0;
+  TS.query.matches(tree.rootNode).forEach((match) => {
+    const className = match.captures[0].name.replaceAll(".", "-");
+    const text = match.captures[0].node.text;
+    const start = match.captures[0].node.startIndex;
+    const end = match.captures[0].node.endIndex;
 
-      adjusted += `<span class="${className}">${text}</span>`;
-      lastEnd = end;
-    });
-
-    if (lastEnd < code.length) {
-      adjusted += code.substring(lastEnd);
+    if (start < lastEnd) {
+      return; // avoid duplicate matches for the same text
     }
-    return adjusted;
-  }
+    if (start > lastEnd) {
+      adjusted += code.substring(lastEnd, start);
+    }
 
-  window.TS = {
-    parser: null,
-    activeLanguage: null,
-    query: null,
-    setLanguage,
-    highlight,
-  };
-});
+    adjusted += `<span class="${className}">${text}</span>`;
+    lastEnd = end;
+  });
+
+  if (lastEnd < code.length) {
+    adjusted += code.substring(lastEnd);
+  }
+  return adjusted;
+}
+
+window.TS = {
+  parser: null,
+  activeLanguage: null,
+  query: null,
+  setLanguage,
+  highlight,
+};
